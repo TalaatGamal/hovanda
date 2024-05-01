18437ce78d1912fd1e5ed2efd763a9baf421b04f
TalaatGamal <talatgamal890@gmail.com>
2024-05-01 07:14:55 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,26 @@
+
+
+                                //  Comments in js⭕
+
+// single line comment
+
+// single line comment
+
+// single line comment
+
+
+/*
+multiple line comment
+again
+again
+again
+
+*/ 
+
+                               // comments in js⭕❗
+
+                               console.log("log");
+                               console.error("error")
+                               console.table(["sat" ,"sun" , "mon"]);
+                            //    alert("Talaat Gamal")
+
